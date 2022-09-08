4e7c43bc8647bca226bf2000fdf81278fae239e4
isdiscodead <isdiscodead@korea.ac.kr>
2022-09-08 13:48:42 +0900

    use container

--- without-redux/src/Components/AddNumberRoot.jsx
+++ without-redux/src/Components/AddNumberRoot.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import AddNumber from './AddNumber';
+import AddNumber from '../Containers/AddNumber'; // 컨테이너로 바꿔치기 
 
 export default class AddNumberRoot extends Component {
     render() {
--- without-redux/src/Components/DisplayNumber.jsx
+++ without-redux/src/Components/DisplayNumber.jsx
@@ -6,7 +6,7 @@ export default class DisplayNumber extends Component {
   constructor(props) {
     super(props); // 반드시 먼저 실행 ! 
     store.subscribe(function() {
-      this.setState({number: store.getState().number});
+      this.setState({number: store.getState().number}); // 변경 시 render() 재 호출을 위한 subscribe
     }.bind(this));
   }
     render() {
